6eff3059b375bab072bc6dfd006fd79257bf3233
krystiano13 <crystianotv@gmail.com>
2024-02-08 20:49:35 +0100

    simplify Footer component

---
+++ client/src/partials/Footer/FooterData.ts
@@ -0,0 +1,27 @@
+import Facebook from '../../images/icon-facebook.svg';
+import Instagram from '../../images/icon-instagram.svg';
+import Pinterest from '../../images/icon-pinterest.svg';
+import Twitter from '../../images/icon-twitter.svg';
+
+export const Links = [
+    "Features", "Resources", "Company"
+];
+
+export const Logos = [
+    {
+        src: Facebook,
+        alt: "facebook logo"
+    },
+    {
+        src: Instagram,
+        alt: "instagram logo"
+    },
+    {
+        src: Pinterest,
+        alt: "pinterest logo"
+    },
+    {
+        src: Twitter,
+        alt: "twitter logo"
+    },
+];
