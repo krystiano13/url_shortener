27177971ba2c4f329b0da2c4abd3bf4b84983f3c
krystiano13 <crystianotv@gmail.com>
2024-02-09 10:29:20 +0100

    create class for database connection

--- server/src/classes/Database.php
+++ server/src/classes/Database.php
@@ -4,5 +4,37 @@ namespace App\classes;
 
 class Database
 {
+    private array $config = [];
     public $connection;
+
+    private function getConfig() {
+        return require_once '../configs/db.php';
+    }
+
+    public function __construct()
+    {
+        $this -> config = $this -> getConfig();
+
+        $host = $this -> config['host'];
+        $port = $this -> config['port'];
+        $dbname = $this -> config['dbname'];
+
+        $this -> connection = new \PDO(
+            "mysql:host={$host};port={$port};dbname={$dbname}",
+            $this -> config['user'],
+            $this -> config['password'],
+            [
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_EMULATE_PREPARES => false
+            ]
+        );
+    }
+
+    public function query(string $sql, array $params = [])
+    {
+        $query = $this -> connection -> prepare($sql);
+        $query -> execute($params);
+        return $query -> fetchAll();
+    }
 }
