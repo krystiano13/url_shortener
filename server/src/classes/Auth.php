283ab72992d46225b5fc9af8a9ee2fd3c2e3ad7f
krystiano13 <crystianotv@gmail.com>
2024-02-09 13:49:01 +0100

    create Auth class

---
+++ server/src/classes/Auth.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\classes;
+
+use App\classes\Database;
+
+class Auth
+{
+    public static function passwordConfirm(string $password1, string $password2):bool {
+        return $password1 === $password2;
+    }
+
+    public static function passwordLength(string $password, int $min):bool {
+        return strlen($password) >= $min;
+    }
+
+    public static function unique(string $value, string $column):bool {
+        $db = new Database();
+        $result = $db -> query("SELECT {$column} from users WHERE {$column} = :value", [
+            'value' => $value
+        ]);
+
+        if($result[0] && count($result) > 0) {
+            return false;
+        }
+
+        if(!$result[0] || count($result) === 0) {
+            return true;
+        }
+    }
+}
