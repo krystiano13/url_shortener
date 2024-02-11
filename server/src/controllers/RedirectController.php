569b694c4d98679b4a2f1c1d805eab7a33c21ca0
krystiano13 <crystianotv@gmail.com>
2024-02-11 11:11:52 +0100

    create controller for redirecting with shortened url

--- server/src/controllers/RedirectController.php
+++ server/src/controllers/RedirectController.php
@@ -2,7 +2,27 @@
 
 namespace App\controllers;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+use App\classes\Database;
+
 class RedirectController
 {
+    private string $url;
+    public function getUrl(Request $request, Response $response,array $args) {
+        $this -> url = $args['url'];
 
+        $db = new Database();
+        $data = $db -> query('SELECT longURL from urls WHERE shortURL=:url',[
+            'url' => $this -> url
+        ]);
+
+        if(count($data)) {
+            header("location: {$data[0]['longURL']}");
+            exit();
+        }
+        $response -> getBody() -> write("URL not fount 404");
+        return $response;
+    }
 }
