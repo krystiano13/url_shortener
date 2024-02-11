651b65890d1a29b867d03dd2fc6a16f5d090f59d
krystiano13 <crystianotv@gmail.com>
2024-02-11 10:38:38 +0100

    create function for shortening the url

--- server/src/controllers/LinkController.php
+++ server/src/controllers/LinkController.php
@@ -19,7 +19,7 @@ class LinkController
 
         $username = $body['username'];
         $token = $body['token'];
-        $url = $body['url'];
+        $this -> longUrl = $body['url'];
 
         if(!AuthMiddleware::auth($token,$username)) {
             $response -> getBody() -> write(json_encode(['errors' => ['Unauthorized']]));
@@ -27,5 +27,27 @@ class LinkController
                 -> withHeader('Content-Type', 'application/json')
                 -> withStatus(403);
         }
+
+        if(!Validator::url($this -> longUrl)) {
+            $response -> getBody() -> write(json_encode(['errors' => ['Value is not an URL']]));
+            return $response
+                -> withHeader('Content-Type', 'application/json')
+                -> withStatus(401);
+        }
+
+        $this -> shortUrl = $this -> createURL();
+    }
+
+    private function createURL():string {
+        return substr(
+            base64_encode(
+                sha1(uniqid(
+                    random_bytes(self::RANDOM_BYTES),
+                    true
+                ))
+            ),
+            0,
+            self::URL_LENGHT
+        );
     }
 }
