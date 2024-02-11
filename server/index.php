96d890b2933106630f11aff7d394d881a94b1871
krystiano13 <crystianotv@gmail.com>
2024-02-11 10:31:54 +0100

    implement veryfing user on LinkController

--- server/index.php
+++ server/index.php
@@ -6,18 +6,13 @@ header("Access-Control-Allow-Headers: *");
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use App\classes\Database;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-
-    $db = new Database();
-    $result = $db -> query(
-        "SELECT * FROM users"
-    );
+    $result = "Hello World";
     $response->getBody()->write(json_encode($result));
     return $response -> withHeader('Content-Type', 'application/json');
 });
--- server/src/controllers/LinkController.php
+++ server/src/controllers/LinkController.php
@@ -4,10 +4,25 @@ namespace App\controllers;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\middleware\AuthMiddleware;
 
 class LinkController
 {
+    private const URL_LENGHT = 5;
+    private const RANDOM_BYTES = 32;
+    private string $longUrl;
+    private string $shortUrl;
     public function shorten(Request $request, Response $response) {
+        $body = $request -> getBody();
 
+        $username = $body['username'];
+        $token = $body['token'];
+
+        if(!AuthMiddleware::auth($token,$username)) {
+            $response -> getBody() -> write(json_encode(['errors' => ['Unauthorized']]));
+            return $response
+                -> withHeader('Content-Type', 'application/json')
+                -> withStatus(403);
+        }
     }
 }
