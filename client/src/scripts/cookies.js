8fd2de708c3a7d6efa52e27eefa0938cddde1dfd
krystiano13 <crystianotv@gmail.com>
2024-02-13 17:56:31 +0100

    create function for getting and deleting cookies

--- client/src/scripts/cookies.js
+++ client/src/scripts/cookies.js
@@ -2,5 +2,22 @@ const setCookie = (name,value,expires) => {
     const date = new Date();
     date.setTime(date.getTime() + expires * 24 * 60 * 60 * 1000);
     const expire = `expires=${date.toUTCString()}`;
-    document.cookie = `${name}=${value};${expire}`;
+    document.cookie = `${name}=${value}; ${expire}; path=/`;
+}
+
+const deleteCookie = (name) => {
+    setCookie(name, null, null);
+}
+
+const getCookie = (name) => {
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookies = decodedCookie.split("; ");
+
+    cookies.forEach(item => {
+        if(item.indexOf(name) == 0) {
+            return item.substring(name.length + 1);
+        }
+    });
+
+    return null;
 }
